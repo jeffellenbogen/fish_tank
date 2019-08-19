98583670a51f43669b4c6fd540d742857b2b715b
Jeff Ellenbogen <jeffellenbogen@gmail.com>
2019-08-19 12:59:09 -0600

    Update clownfish_tank.py

--- clownfish_tank.py
+++ clownfish_tank.py
@@ -124,7 +124,7 @@ try:
 
     screen_draw.text((edge_offset_x,total_rows - edge_offset_y * 2),time_string, fill = (219,4,216), font = fnt2)
     screen_draw.text((edge_offset_x, total_rows - edge_offset_y),day_of_week, fill = (255,0,0), font = fnt)
-    screen_draw.text((edge_offset_x + day_of_week_size[0] + text_spacing, total_rows - edge_offset_y),date_string, fill = (0,0,255), font = fnt)
+    screen_draw.text((edge_offset_x + day_of_week_size[0] + text_spacing, total_rows - edge_offset_y),date_string, fill = (255,0,255), font = fnt)
     matrix.SetImage(screen,0,0)
 
     # update our location for next time
