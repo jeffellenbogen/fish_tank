7cd51c9871374937620225ee7aca4fc1d46c7c66
Jeff Ellenbogen <jeffellenbogen@gmail.com>
2019-08-19 11:55:24 -0600

    Update space_tank_text_bottom.py

--- space_tank_text_bottom.py
+++ space_tank_text_bottom.py
@@ -117,12 +117,13 @@ try:
     #date_vertical_offset = -17
     #date_y = time_y + date_vertical_offset
 
-    edge_offset = 10
+    edge_offset_x = 8
+    edge_offset_y = 15
     text_spacing = 5
 
-    screen_draw.text((edge_offset,total_rows - edge_offset),time_string, fill = (219,4,216), font = fnt)
-    screen_draw.text((edge_offset + time_size[0]+ text_spacing, total_rows - edge_offset),day_of_week, fill = (255,255,255), font = fnt)
-    screen_draw.text((edge_offset + time_size[0] + text_spacing * 2+ day_of_week_size[0], total_rows - edge_offset),date_string, fill = (245,245,66), font = fnt)
+    screen_draw.text((edge_offset_x,total_rows - edge_offset_y),time_string, fill = (219,4,216), font = fnt)
+    screen_draw.text((edge_offset_x + time_size[0]+ text_spacing, total_rows - edge_offset_y),day_of_week, fill = (255,255,255), font = fnt)
+    screen_draw.text((edge_offset_x + time_size[0] + text_spacing * 2+ day_of_week_size[0], total_rows - edge_offset_y),date_string, fill = (245,245,66), font = fnt)
     matrix.SetImage(screen,0,0)
 
     # update our location for next time
