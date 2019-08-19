d54f8562964914238af91068eaa129d1fab6dbe9
Jeff Ellenbogen <jeffellenbogen@gmail.com>
2019-08-19 12:43:59 -0600

    Update clownfish_tank.py

--- clownfish_tank.py
+++ clownfish_tank.py
@@ -73,7 +73,7 @@ for item in icon_data:
 mask.putdata(mask_data)
 
 icon_x = total_columns
-icon_y = random.randint(0,total_rows-icon_size)
+icon_y = random.randint(0,total_rows-icon_height)
 
 screen = Image.new("RGBA",(total_columns,total_rows))
 
