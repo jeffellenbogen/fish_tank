18557956fd7b77d08ed0d849467ae8018fdbe55b
Jeff Ellenbogen <jeffellenbogen@gmail.com>
2019-08-19 12:53:59 -0600

    greenscreen testing

--- clownfish_tank.py
+++ clownfish_tank.py
@@ -64,7 +64,7 @@ icon_data = icon_image.getdata()
 mask_data = []
 for item in icon_data:
   print item
-  if item[0] == 255 and item[1] == 255 and item[2] == 255:
+  if item[0] == 0 and item[1] == 255 and item[2] == 0:
     mask_data.append(0)
     print "transparent"
   else:
