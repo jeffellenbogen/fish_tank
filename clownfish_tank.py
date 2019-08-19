c5ba03a79c54aab0869b6a6196b514b095fa7616
Jeff Ellenbogen <jeffellenbogen@gmail.com>
2019-08-19 15:21:11 -0600

    Update clownfish_tank.py

--- clownfish_tank.py
+++ clownfish_tank.py
@@ -64,7 +64,7 @@ icon_data = icon_image.getdata()
 mask_data = []
 for item in icon_data:
   print item
-  if item[0] <= 20 and item[1] >= 235 and item[2] <= 20:
+  if item[0] <= 10 and item[1] >= 245 and item[2] <= 10:
     mask_data.append(0)
     print "transparent"
   else:
@@ -82,11 +82,21 @@ screen = Image.new("RGBA",(total_columns,total_rows))
 # Clownfish direction chooser
 #############################################
 def clownfishDirectionChooser():
-  clownfish_direction_chooser = random.randint(0,2)
-  if clownfish_direction_chooser == 0:
+  global icon_x
+  global icon_y
+  global total_columns
+  global total_rows
+  global icon_height
+
+  clownfish_direction_chooser = random.randint(1,10)
+  if clownfish_direction_chooser % 2 == 0:
+    icon_x = total_columns
+    icon_y = random.randint(0,total_rows-icon_height)
     print "swim left"
     return -1
   else: 
+    icon_x = 0
+    icon_y = random.randint(0,total_rows-icon_height)
     print "swim right"
     return 1
 
@@ -114,26 +124,11 @@ try:
     time_string = currentDT_TZadjusted.strftime("%I:%M:%S %p")
     day_of_week = currentDT_TZadjusted.strftime("%A")
     date_string = currentDT_TZadjusted.strftime("%B %d, %Y")
-    '''How can we add the current date in the form Day-of-week, Month, Day-of-Month, Year
-    Also how can we adjust for the Colorado Time Zone?
-    Does this datetime.datetime.now pull from an internet time source or the local time on the Rasp Pi/Computer?
-    How do we adjust for daylight savings?'''
 
-    # do some math to center our time string
+
     time_size = fnt2.getsize(time_string)
     day_of_week_size = fnt.getsize(day_of_week)
     date_string_size = fnt.getsize(date_string)
-
-    #time_x = (total_columns - time_size[0])/2
-    #time_y = (total_rows - time_size[1])/2 
-    
-    #day_x = (total_columns - day_of_week_size[0])/2
-    #day_vertical_offset = -40
-    #day_y = time_y + day_vertical_offset
-
-    #date_x = (total_columns - date_string_size[0])/2
-    #date_vertical_offset = -17
-    #date_y = time_y + date_vertical_offset
 
     edge_offset_x = 3
     edge_offset_y = 13
@@ -144,18 +139,25 @@ try:
     screen_draw.text((edge_offset_x + day_of_week_size[0] + text_spacing, total_rows - edge_offset_y),date_string, fill = (255,255,255), font = fnt)
     matrix.SetImage(screen,0,0)
 
+
     # update our location for next time
     icon_x = icon_x + clownfish_direction
-    if (icon_x < (0 - icon_width)) & clownfish_direction == -1:
-      icon_x = total_columns
-      icon_y = random.randint(0,total_rows-icon_height)
-      clownfish_direction = clownfishDirectionChooser()
-    elif (icon_x > (total_columns + icon_width)) & clownfish_direction == 1:
-      icon_x = 0
-      icon_y = random.randint(0,total_rows-icon_height)
-      clownfish_direction = clownfishDirectionChooser()    
-
-
+    if clownfish_direction == -1:
+      if icon_x < (0 - icon_width):
+        clownfish_direction = clownfishDirectionChooser() 
+        if clownfish_direction == -1:
+          icon_x = total_columns
+        else: 
+          icon_x = 0
+        icon_y = random.randint(0,total_rows-icon_height)
+    else:
+      if icon_x > total_columns + icon_width:
+        clownfish_direction = clownfishDirectionChooser() 
+        if clownfish_direction == -1:
+          icon_x = total_columns
+        else: 
+          icon_x = 0
+        icon_y = random.randint(0,total_rows-icon_height)
     sleep(.1)
 
 except KeyboardInterrupt:
