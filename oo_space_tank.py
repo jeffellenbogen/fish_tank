0f797420bab97ed174913da857270cc6344ad270
Jeff Ellenbogen <jeffellenbogen@gmail.com>
2019-08-29 10:39:29 -0600

    Update oo_space_tank.py

--- oo_space_tank.py
+++ oo_space_tank.py
@@ -69,9 +69,10 @@ class Icon():
   # setSlowdown method 
   #   pastes the icon into the passed image
   ###############################################
+  '''
   def setSlowdown(self,slowdown):
     self.slowdown = slowdown
-
+  '''
 
   ############################################
   # move 
@@ -186,7 +187,7 @@ space_tank = Tank(matrix_rows, matrix_columns, num_horiz, num_vert)
 space_tank.set_background("images/andr_small.jpeg")
 tie = Icon("images/tie-fighter-01.jpg",(242,242),(242,242),(242,242),40,40,space_tank.total_columns,space_tank.total_rows)
 fish = Icon("images/clownfish_left.jpg",(0,10),(200,255),(0,10),40,25,space_tank.total_columns,space_tank.total_rows)
-fish.setSlowdown(2)
+fish.slowdown=2
 space_tank.add_icon(tie)
 space_tank.add_icon(fish)
 
