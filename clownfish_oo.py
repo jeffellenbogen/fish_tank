584853098799aa8779112227b0ff3944ebbd7eaf
Jeff Ellenbogen <jeffellenbogen@gmail.com>
2019-09-08 21:58:11 -0600

    add randomization for tank

--- clownfish_oo.py
+++ clownfish_oo.py
@@ -273,7 +273,13 @@ num_vert = 3
 
 #create an instance of the Tank class and set it to a specific background image
 fish_tank = Tank(matrix_rows, matrix_columns, num_horiz, num_vert)
-fish_tank.set_background("images/reef_bgrd_dark_bottom.jpg")
+tankChooser = random.randint(1,4)
+if tankChooser == 1:
+  fish_tank.set_background("images/reef_bgrd_dark_bottom.jpg")
+elif tankChooser == 2:
+  fish_tank.set_background("images/caribbean-coral-reef.jpg")
+else:
+  fish_tank.set_background("images/coral_tank.jpg")
 
 #create as many instances of the Icon class as needed
 clownfish = Icon("images/clownfish.jpg",(0,10),(150,255),(0,10),40,25,2,fish_tank.total_columns,fish_tank.total_rows)
